9465a802a729edf5d07b126d52eff0c81c9f0bb2
Raditss <raditya.naufal.a@gmail.com>
2022-10-28 08:06:04 +0700

    splash screen

---
+++ src/Function/splash.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void splash()
+{
+    FILE *fptr;
+    fptr=fopen("../../bin/bmo.txt", "r");
+    if (fptr == NULL)
+    {
+        printf("Cannot open file \n");
+        exit(0);
+    }
+    char filename[100], c;
+    // Read contents from file
+    c = fgetc(fptr);
+    while (c != EOF)
+    {
+        printf ("%c", c);
+        c = fgetc(fptr);
+    }
+  
+    fclose(fptr);
+}
+
+int main()
+{
+    splash();
+    return 0;
+}

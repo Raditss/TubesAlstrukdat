99d3fdbb29aa59103bb09420e8219dfaa32b93d2
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2022-11-05 13:48:04 +0700

    Mangat nubesnya kaakk

--- src/Function/simulator.c
+++ src/Function/simulator.c
@@ -10,11 +10,11 @@ int main(){
     FoodType val;
     Simulator sim;
     Word nama_sim = strToWord("Willy.");
-    Posisi awal;
+    Matrix awal = getPeta("../../bin/peta.txt");
+    awal = createBorder(awal);
     NOTIF_STACK notifikasi;
     CreateEmptyNotif(&notifikasi);
 
-    CreatePosisi(&awal, 3, 2);
     createSimulator(&sim, nama_sim, awal);
 
     printf("================================================\n");
@@ -38,5 +38,34 @@ int main(){
     displaySimulator(sim);
     printf("================================================\n");
 
+    realTime = NextNMinute(realTime, 15);
+    removeExpired(&sim, realTime, &notifikasi);
+
+    printf("================================================\n");
+    displaySimulator(sim);
+    printf("================================================\n");
+
+    printf("================================================\n");
+    printf("================================================\n\n");
+
+    DequeueFood(&Daftar,  &val);
+    addInventory(&sim, Info(val), realTime, &notifikasi);
+    DequeueFood(&Daftar,  &val);
+    addInventory(&sim, Info(val), realTime, &notifikasi);
+    DequeueFood(&Daftar,  &val);
+    addInventory(&sim, Info(val), realTime, &notifikasi);
+
+    displaySimulator(sim);
+    Makanan Minyak_Goreng;
+    ID(Minyak_Goreng) = 15;
+    NAMA(Minyak_Goreng) = strToWord("Minyak Goreng.");
+    EXP(Minyak_Goreng) = MakeTIME(0,1,30);
+    DTIME(Minyak_Goreng) = MakeTIME(0,1,0);
+    LOC(Minyak_Goreng) = strToWord("Buy.");
+    removeInventory(&sim, Minyak_Goreng);
+
+    printf("================================================\n");
+
+    displaySimulator(sim);
     return 0;
 }
